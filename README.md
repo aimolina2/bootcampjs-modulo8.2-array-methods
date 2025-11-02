b8542cd2e37bc45a302fb4e2ee5346b6f1bc8f36
Ainoa <aimmolina2@gmail.com>
2025-11-02 13:38:29 +0100

    pacientes pediatria ready

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# typescript-sandbox
+# Laboratorio módulo 8.2 - Array methods
 
 Para la parte de cada módulo en la que te planteamos ejercicios con JavaScript puedes usar este sandbox.
 
@@ -8,4 +8,11 @@ Pasos:
 - Instala las dependencias con `npm install`.
 - Ejecuta el sandbox con `npm run dev`.
 - Abre el navegador en `http://localhost:5173/` (si ese puerto no te funciona, mira en la consola donde has hecho el build, puede que este ocupado y se haya abierto en otro puerto).
-- Puedes empezar a meter tu código en el fichero:
+
+## 1. Apartado 1
+
+a. Lista de pacientes asignados a la especialidad _Pediatría_.
+
+b. Lista de pacientes asignados a _Pediatría_ y cuya edad sea menor de 10 años.
+
+Para ambos casos usamos _filter_ para que se nos devuelva un nuevo array con los elementos que cumplen con la condición establecida.
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,101 @@
-import "./style.css";
+type Especialidad = "Medico de familia" | "Pediatra" | "Cardiólogo";
 
-console.log("Hello Typescript!");
+interface Pacientes {
+  id: number;
+  nombre: string;
+  apellidos: string;
+  sexo: string;
+  temperatura: number;
+  frecuenciaCardiaca: number;
+  especialidad: Especialidad;
+  edad: number;
+}
+
+const pacientes: Pacientes[] = [
+  {
+    id: 1,
+    nombre: "John",
+    apellidos: "Doe",
+    sexo: "Male",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 80,
+    especialidad: "Medico de familia",
+    edad: 44,
+  },
+  {
+    id: 2,
+    nombre: "Jane",
+    apellidos: "Doe",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 70,
+    especialidad: "Medico de familia",
+    edad: 43,
+  },
+  {
+    id: 3,
+    nombre: "Junior",
+    apellidos: "Doe",
+    sexo: "Male",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 90,
+    especialidad: "Pediatra",
+    edad: 8,
+  },
+  {
+    id: 4,
+    nombre: "Mary",
+    apellidos: "Wien",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 120,
+    especialidad: "Medico de familia",
+    edad: 20,
+  },
+  {
+    id: 5,
+    nombre: "Scarlett",
+    apellidos: "Somez",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 110,
+    especialidad: "Cardiólogo",
+    edad: 30,
+  },
+  {
+    id: 6,
+    nombre: "Brian",
+    apellidos: "Kid",
+    sexo: "Male",
+    temperatura: 39.8,
+    frecuenciaCardiaca: 80,
+    especialidad: "Pediatra",
+    edad: 11,
+  },
+];
+
+// Apartado 1.a. Lista de pacientes asignados a Pediatría
+
+const obtenPacientesAsignadosAPediatria = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  return pacientes.filter((paciente) => paciente.especialidad === "Pediatra");
+};
+console.log(
+  "Listado de pacientes de la especialidad Pediatría:",
+  obtenPacientesAsignadosAPediatria(pacientes)
+);
+
+// Apartado 1.b. Lista de pacientes asignados a Pediatría y cuya edad sea menor de 10 años.
+
+const obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  return pacientes.filter(
+    (paciente) => paciente.especialidad === "Pediatra" && paciente.edad < 10
+  );
+};
+console.log(
+  "Listado de pacientes de la especialidad Pediatría con menos de 10 años:",
+  obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(pacientes)
+);
